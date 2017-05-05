6a521c6d7771928655803c755d3841878107b263
macarena baltra <macarena.bc2011@gmail.com>
2017-05-04 20:52:00 -0400

    Se crea el constructor y se crean nuevas personas

--- asetss/javascript.js
+++ asetss/javascript.js
@@ -1,15 +1,14 @@
-function Pokemon(nombre,color){
+function MiembroSquad(nombre,edad,hobbie1, hobbie2, hobbie3){
     this.nombre = nombre;
-    this.color = color;
-
-    this.nivelDeAmistad = 0;
-
-    this.mostrarPokemon = function(){
-      return('Hola, soy: ' + this.nombre + ' y soy de color: ' + this.color)
-    }
-
-    this.aumentarAmistad = function(valor){
-      this.nivelDeAmistad = this.nivelDeAmistad + valor;
-    }
+    this.edad = edad;
+    this.hobbie1 = hobbie1;
+    this.hobbie2 = hobbie2;
+    this.hobbie3 = hobbie3;
 }
-const Pikachu = new Pokemon('Pikachu','amarillo');
+var bere = new MiembroSquad('Berenisse Rios','28', 'Yoga', 'Viajar', 'Leer');
+var su = new MiembroSquad('Susana Arce','28', 'Ficción', 'Dar golpesitos con cariño', 'Marketing');
+var dani = new MiembroSquad('Daniela Medina','20', 'Acupuntura', 'Danza Contemporanea', 'Ir al kioskito');
+var rata = new MiembroSquad('Andrea Ratika','24', 'Leer', 'Aprender', 'Dibujar');
+var nico = new MiembroSquad('Nicole Aliste','32', 'Comida China', 'Juegos de video', 'Andar Estilosa');
+var clau = new MiembroSquad('Claudia Rivera','33', 'Yoga', 'Patinar', 'Comer Dulces');
+var fran = new MiembroSquad('Francisca Ruiz-Tagle','35', 'Perros', 'Ilustración', 'Manualidades');
