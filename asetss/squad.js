aa32f338eaf582642a453c74db00f4ad962fb14c
macarena baltra <macarena.bc2011@gmail.com>
2017-05-04 23:14:01 -0400

    Se sube el ejercicio práctico 2 y se agregan cambios de diseño

--- asetss/squad.js
+++ asetss/squad.js
@@ -16,20 +16,31 @@ var impre = document.getElementById('listita');
 
 var res1 = "<b><li>Nombre y Apellido: </b>" + bere.nombre + "<br></li><b><li>Edad: </b>" + bere.edad + "<br></li><b><li>Hobbie: </b>" + bere.hobbie
     + "<br><br>";
-var res2 = "<b><li>Nombre y Apellido: </b>" + su.nombre + "<br></li><b><li>Edad: </b>" + su.edad + "<br></li><b><li>Hobbie: </b>" + su.hobbie
+var res2 = "--------------------------------------<b><li>Nombre y Apellido: </b>" + su.nombre + "<br></li><b><li>Edad: </b>" + su.edad + "<br></li><b><li>Hobbie: </b>" + su.hobbie
     + "<br><br>";
-var res3 = "<b><li>Nombre y Apellido: </b>" + dani.nombre + "<br></li><b><li>Edad: </b>" + dani.edad + "<br></li><b><li>Hobbie: </b>" + dani.hobbie
+var res3 = "--------------------------------------<b><li>Nombre y Apellido: </b>" + dani.nombre + "<br></li><b><li>Edad: </b>" + dani.edad + "<br></li><b><li>Hobbie: </b>" + dani.hobbie
     + "<br><br>";
-var res4 = "<b><li>Nombre y Apellido: </b>" + rata.nombre + "<br></li><b><li>Edad: </b>" + rata.edad + "<br></li><b><li>Hobbie: </b>" + rata.hobbie
+var res4 = "--------------------------------------<b><li>Nombre y Apellido: </b>" + rata.nombre + "<br></li><b><li>Edad: </b>" + rata.edad + "<br></li><b><li>Hobbie: </b>" + rata.hobbie
     + "<br><br>";
-var res5 = "<b><li>Nombre y Apellido: </b>" + nico.nombre + "<br></li><b><li>Edad: </b>" + nico.edad + "<br></li><b><li>Hobbie: </b>" + nico.hobbie
+var res5 = "--------------------------------------<b><li>Nombre y Apellido: </b>" + nico.nombre + "<br></li><b><li>Edad: </b>" + nico.edad + "<br></li><b><li>Hobbie: </b>" + nico.hobbie
     + "<br><br>";
-var res6 = "<b><li>Nombre y Apellido: </b>" + clau.nombre + "<br></li><b><li>Edad: </b>" + clau.edad + "<br></li><b><li>Hobbie: </b>" + clau.hobbie
+var res6 = "--------------------------------------<b><li>Nombre y Apellido: </b>" + clau.nombre + "<br></li><b><li>Edad: </b>" + clau.edad + "<br></li><b><li>Hobbie: </b>" + clau.hobbie
     + "<br><br>";
-var res7 = "<b><li>Nombre y Apellido: </b>" + fran.nombre + "<br></li><b><li>Edad: </b>" + fran.edad + "<br></li><b><li>Hobbie: </b>" + fran.hobbie
+var res7 = "--------------------------------------<b><li>Nombre y Apellido: </b>" + fran.nombre + "<br></li><b><li>Edad: </b>" + fran.edad + "<br></li><b><li>Hobbie: </b>" + fran.hobbie
     + "<br><br>";
 
 var resComprimido = res1 + res2 + res3 + res4 + res5 + res6 + res7;
 
-
 impre.innerHTML = resComprimido;
+
+// Hora - extra
+var fechaHora = new Date();
+var horas = fechaHora.getHours();
+var minutos = fechaHora.getMinutes();
+var segundos = fechaHora.getSeconds();
+
+if(horas < 10) { horas = '0' + horas; }
+if(minutos < 10) { minutos = '0' + minutos; }
+if(segundos < 10) { segundos = '0' + segundos; }
+
+document.getElementById('reloj').innerHTML = "La hora de tu visita es: " + horas +":" + minutos + ":" + segundos + " hrs.";
